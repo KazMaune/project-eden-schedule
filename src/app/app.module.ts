7a18d93f0295be305e5298ef7274e64f5440c6c0
Kikorono <kikorono.vr@gmail.com>
2022-11-12 14:29:34 -0600

    Adding navbar module and component to split out html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarModule } from './navbar/navbar.module';
 
 @NgModule({
     declarations: [
@@ -12,7 +13,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     imports: [
         BrowserModule,
         AppRoutingModule,
-        NgbModule
+        NgbModule,
+        NavbarModule
     ],
     providers: [],
     bootstrap: [ AppComponent ]
