19482bc87ab75c74a159b678e7a1d6256fe35995
Kikorono <kikorono.vr@gmail.com>
2022-11-17 17:33:14 -0600

    Implementing navbar routing links

--- src/app/components/navbar/navbar.module.ts
+++ src/app/components/navbar/navbar.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './navbar.component';
 
@@ -13,6 +14,7 @@ import { NavbarComponent } from './navbar.component';
     ],
     imports: [
         CommonModule,
+        RouterModule,
         NgbDropdownModule
     ]
 })
